29a94b9af186364350595f2f722af78b6704a3ea
willian <willian.chris.89@gmail.com>
2022-02-11 14:22:04 -0300

    resolução do exercício de arrays da ju

--- array nivelamento.js
+++ array nivelamento.js
@@ -61,6 +61,28 @@ for (i = inicio; i <= final; i++) {
     var nome = pessoas[i].nome
     var idade = pessoas[i].idade
     var rg = pessoas[i].rg
-
-    console.log(`Nome: ${nome}\nIdade: ${idade}\nRG: [${rg}]\n`)
+    var cpf = pessoas[i].cpf
+    var rua = pessoas[i].endereco.rua
+    var numero = pessoas[i].endereco.numero
+    var bairro = pessoas[i].endereco.bairro
+    var cidade = pessoas[i].endereco.cidade
+    var estado = pessoas[i].endereco.estado
+    var cep = pessoas[i].endereco.cep
+    console.log(`\nNome: ${nome}\nIdade: ${idade}\nRG: [${rg}] - CPF: ${cpf}\n\nEndereço: \nRua: ${rua}, ${numero}, ${bairro}, ${cidade} / ${estado}\nCEP: ${cep} `
+    )
 }
+/*var inicio = 0
+// pegar ultimo da lista
+var final = endereco.length - 1
+
+for (i = inicio; i <= final; i++) {
+    var rua = endereco[i].rua
+    var numero = endereco[i].numero
+    var bairro = endereco[i].bairro
+    var cidade = endereco[i].cidade
+    var estado = endereco[i].estado
+    var cep = endereco[i].cep
+    console.log(`Rua: ${rua} numero ${numero} bairro ${bairro} cidade ${cidade} estado ${estado} cep ${cep}`)
+}
+
+*/
